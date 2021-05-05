a1d98dfc119038dea7b032822019b16e35eb9121
Adrien Fabre <adrien.fabre@dashlane.com>
2021-05-05 23:18:02 +0200

    Time and  position gradient

--- sketch.js
+++ sketch.js
@@ -12,17 +12,21 @@ const sketch = (p5) => {
             data: appData
         });
 
-        const canvas = p5.createCanvas(800, 800, p5.WEBGL);
+        const canvas = p5.createCanvas(window.innerWidth * 0.9, window.innerHeight * 0.9, p5.WEBGL);
         canvas.parent('canvasDiv');
         p5.colorMode(p5.HSB, 100);
-        p5.setAttributes('antialias', true);
+        // p5.setAttributes('antialias', true);
     };
 
+    let cpt = 0;
     p5.draw = () => {
+        cpt += 0.01;
         gradientShader.setUniform('u_c1', [0.25, 0.54, 0.54]);
         gradientShader.setUniform('u_c2', [0.09, 0.63, 0.42]);
+        gradientShader.setUniform('u_time', cpt);
 
         gradientShader.setUniform('u_resolution', [p5.width, p5.height]);
+        // gradientShader.setUniform('u_resolution', [p5.width * 2, p5.height * 2]);
         p5.shader(gradientShader);
         p5.rect(0, 0, p5.width, p5.height);
     };

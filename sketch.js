9f10926e5b577cf58b4d4819c1ea9da07e6a8ad7
Adrien Fabre <adrien.fabre@dashlane.com>
2021-05-11 00:16:18 +0200

    Simple random shader with update function

--- sketch.js
+++ sketch.js
@@ -5,12 +5,20 @@ const sketch = (p5) => {
     let appData = {};
     let init = true;
     let seed;
+    let cpt = 0;
+
+    const updateSimulation = (init, time) => {
+        shader.setUniform('u_resolution', [p5.width, p5.height]);
+        shader.setUniform('u_texture', graphics);
+
+        shader.setUniform('u_init', cpt === 1);
+        shader.setUniform('u_time', cpt);
+
+        graphics.rect(0, 0, p5.width, p5.height);
+    };
 
     p5.preload = () => {
-        gradientShader = p5.loadShader('shader/uniform.vert', 'shader/gradient2.frag');
-        rainbowShader = p5.loadShader('shader/uniform.vert', 'shader/rainbow.frag');
-        halfUniformShader = p5.loadShader('shader/uniform2.vert', 'shader/uniform.frag');
-        golShader = p5.loadShader('shader/uniform2.vert', 'shader/gol.frag');
+        shader = p5.loadShader('shader/uniform2.vert', 'shader/random.frag');
     };
 
     p5.setup = () => {
@@ -23,33 +31,16 @@ const sketch = (p5) => {
         canvas.parent('canvasDiv');
         p5.setAttributes('antialias', true);
 
-        // boardDimensions = [p5.width / 10, p5.height / 10];
+        graphics = p5.createGraphics(...boardDimensions, p5.WEBGL);
+        graphics.shader(shader);
 
-        golGraphics = p5.createGraphics(...boardDimensions, p5.WEBGL);
-        golGraphics.shader(golShader);
-
-        seed = Math.random();
+        setInterval(updateSimulation, 100);
     };
 
-    let cpt = 0;
     p5.draw = () => {
         cpt += 1;
 
-        golShader.setUniform('u_time', cpt);
-        golShader.setUniform('u_seed', seed);
-        golShader.setUniform('u_resolution', boardDimensions);
-        golShader.setUniform('u_update', cpt % 10 === 0);
-
-        if (init) {
-            golShader.setUniform('u_init', true);
-            init = false;
-        } else {
-            golShader.setUniform('u_init', false);
-            golShader.setUniform('u_texture', golGraphics);
-        }
-
-        golGraphics.rect(0, 0, ...boardDimensions);
-        p5.texture(golGraphics);
+        p5.texture(graphics);
         p5.background('black');
         p5.rect(-p5.width / 2, -p5.height / 2, p5.width, p5.height);
     };
